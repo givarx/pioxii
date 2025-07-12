5b6bb7a50471dcfb26cb69da552145d6e02457be
__giva <157858211+givarx@users.noreply.github.com>
2025-07-12 17:02:45 +0200

    fix: remove shuffling card into the deck from negate effect and update comments

--- script/c600000029.lua
+++ script/c600000029.lua
@@ -1,8 +1,8 @@
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Negate effect and shuffle this card into the Deck
+	-- Negate effect
 	local e1=Effect.CreateEffect(c)
-	   e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY+CATEGORY_TODECK)
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetProperty(0)
@@ -25,7 +25,6 @@ end
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-   Duel.SetOperationInfo(0,CATEGORY_TODECK,e:GetHandler(),1,0,0)  -- Set info for shuffling card into deck
 	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
 	end
@@ -35,13 +34,8 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
 		Duel.Destroy(eg,REASON_EFFECT)
 	end
-	-- Alla fine, rimischia sempre questa carta nel deck
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		Duel.SendtoDeck(c,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
-	end
 end
 function s.cost_func(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	-- Nessun costo aggiuntivo, la carta verrà rimischiata dopo la risoluzione
+	-- Nessun costo aggiuntivo
 end
