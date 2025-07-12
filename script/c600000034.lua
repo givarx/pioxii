c428619e4b7ebd618219452a9af370a281227b1d
__giva <157858211+givarx@users.noreply.github.com>
2025-07-12 22:18:54 +0200

    fix: update special summon effect to correctly trigger on monster leave events and improve cleanup logic

--- script/c600000034.lua
+++ script/c600000034.lua
@@ -13,9 +13,11 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_LEAVE_FIELD)
 	e1:SetRange(LOCATION_GRAVE)
-	e1:SetCode(EVENT_LEAVE_FIELD+EVENT_BE_MATERIAL)
+	e1:SetCountLimit(1,id)
 	e1:SetCondition(s.spcon)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
@@ -24,8 +26,12 @@ end
 
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	-- Controlla se almeno 2 mostri che erano in una zona mostri hanno lasciato il campo
-	local ct=eg:FilterCount(Card.IsPreviousLocation,nil,LOCATION_MZONE)
+	local ct=eg:FilterCount(s.cfilter,nil)
 	return ct>=2 
+end
+
+function s.cfilter(c)
+	return c:IsPreviousLocation(LOCATION_MZONE)
 end
 
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -43,8 +49,12 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetCode(EVENT_PHASE+PHASE_END)
 		e1:SetReset(RESET_PHASE+PHASE_END)
 		e1:SetCountLimit(1)
+		e1:SetLabelObject(c)
 		e1:SetOperation(function(e,tp,eg,ep,ev,re,r,rp)
-			Duel.Remove(c,POS_FACEUP,REASON_EFFECT)
+			local tc=e:GetLabelObject()
+			if tc and tc:IsLocation(LOCATION_MZONE) then
+				Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+			end
 		end)
 		Duel.RegisterEffect(e1,tp)
 	end
