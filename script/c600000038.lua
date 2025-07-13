1486345aa2242138a2bc5290c292262d34ea1435
__giva <157858211+givarx@users.noreply.github.com>
2025-07-13 14:54:06 +0200

    fix: correct location check in special summon target function

--- script/c600000038.lua
+++ script/c600000038.lua
@@ -20,7 +20,7 @@ end
 
 --special summon target
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_FZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
