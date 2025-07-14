1c961173f963ffff76d9f722c49053462ad80fd6
__giva <157858211+givarx@users.noreply.github.com>
2025-07-14 12:20:47 +0200

    fix: refine activation effect and condition checks for special summoning "Colelli"

--- script/c600000031.lua
+++ script/c600000031.lua
@@ -6,12 +6,13 @@
 
 local s, id=GetID()
 function s.initial_effect(c)
-    -- Effetto di attivazione: Se un "Davide" è presente sul terreno, scarta un mostro Macchina per evocare un "Colelli"
+    -- Effetto di attivazione
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCondition(s.condition)
     e1:SetCost(s.cost)
     e1:SetTarget(s.target)
     e1:SetOperation(s.activate)
@@ -19,13 +20,17 @@ function s.initial_effect(c)
 end
 s.listed_names={600000014} -- Colelli
 
+-- Condizione: Controlla se c'è un "Davide" sul terreno
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsExistingMatchingCard(s.davidefilter,tp,LOCATION_MZONE,0,1,nil)
+end
 function s.davidefilter(c)
     return c:IsFaceup() and c:IsSetCard(0x4444) -- Archetipo "Davide"
 end
 
 -- Costo: Scarta un mostro di tipo Macchina dalla mano
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return true end
+    if chk==0 then return Duel.IsExistingMatchingCard(s.machinefilter,tp,LOCATION_HAND,0,1,nil) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
     local g=Duel.SelectMatchingCard(tp,s.machinefilter,tp,LOCATION_HAND,0,1,1,nil)
     Duel.SendtoGrave(g,REASON_COST)
@@ -34,12 +39,10 @@ function s.machinefilter(c)
     return c:IsType(TYPE_MACHINE) and c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
 end
 
--- Target: Controlla tutte le condizioni di attivazione
+-- Target: Controlla se puoi evocare un "Colelli"
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then 
-        return Duel.IsExistingMatchingCard(s.davidefilter,tp,LOCATION_MZONE,0,1,nil)
-            and Duel.IsExistingMatchingCard(s.machinefilter,tp,LOCATION_HAND,0,1,nil)
-            and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
             and Duel.IsExistingMatchingCard(s.colellifilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) 
     end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
