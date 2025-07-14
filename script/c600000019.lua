41d496a712812e6a452e1de19c7cb1a44ed08907
__giva <157858211+givarx@users.noreply.github.com>
2025-07-14 10:15:16 +0200

    fix: remove redundant damage effect registration and ensure consistent damage handling for individual cards

--- script/c600000019.lua
+++ script/c600000019.lua
@@ -24,13 +24,7 @@ function s.initial_effect(c)
 	e2:SetTarget(s.mvtg)
 	e2:SetOperation(s.mvop)
 	c:RegisterEffect(e2)
-	--damage when monster leaves field
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e3:SetCode(EVENT_LEAVE_FIELD)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetOperation(s.damop)
-	c:RegisterEffect(e3)
+	--damage when monster leaves field (removed - using individual effects instead)
 end
 
 --add counter condition
@@ -89,41 +83,27 @@ function s.mvop(e,tp,eg,ep,ev,re,r,rp)
 		local current_counters = tc:GetCounter(0x4321)
 		Debug.Message("Counter attuali su bersaglio: " .. current_counters)
 		
-		--if target has no counters, enable counter support first
+		--enable counter support if needed
 		if current_counters == 0 then
 			tc:EnableCounterPermit(0x4321)
 			Debug.Message("Abilitati counter sulla carta bersaglio: " .. tc:GetCode())
-			
-			--add damage effect to target card when it gets its first counter
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-			e1:SetCode(EVENT_LEAVE_FIELD)
-			e1:SetCondition(s.damagecon)
-			e1:SetOperation(s.damageop2)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-			tc:RegisterEffect(e1)
-			Debug.Message("Effetto di danno applicato alla carta bersaglio: " .. tc:GetCode())
 		end
+		
+		--always add damage effect to ensure it works
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_LEAVE_FIELD)
+		e1:SetCondition(s.damagecon)
+		e1:SetOperation(s.damageop2)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		Debug.Message("Effetto di danno applicato alla carta bersaglio: " .. tc:GetCode())
 		
 		--add counter (works for both cases)
 		tc:AddCounter(0x4321,1)
 		Debug.Message("Counter aggiunto a " .. tc:GetCode() .. "! Totale su bersaglio: " .. tc:GetCounter(0x4321))
 	else
 		Debug.Message("Condizioni non soddisfatte!")
-	end
-end
---damage operation
-function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=eg:GetFirst()
-	while tc do
-		local ct=tc:GetCounter(0x4321)
-		if ct>0 then
-			local p=tc:GetPreviousControler()
-			local damage_amount = ct * 500
-			Debug.Message("Carta " .. tc:GetCode() .. " con " .. ct .. " counter lascia il campo. Danno: " .. damage_amount .. " al giocatore " .. p)
-			Duel.Damage(p,damage_amount,REASON_EFFECT)
-		end
-		tc=eg:GetNext()
 	end
 end
 --damage condition for individual cards
@@ -137,9 +117,9 @@ function s.damageop2(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local ct=c:GetCounter(0x4321)
 	if ct>0 then
-		local p=c:GetPreviousControler()
+		local p=c:GetOwner()
 		local damage_amount = ct * 500
-		Debug.Message("Carta individuale " .. c:GetCode() .. " con " .. ct .. " counter lascia il campo. Danno: " .. damage_amount .. " al giocatore " .. p)
+		Debug.Message("Carta individuale " .. c:GetCode() .. " con " .. ct .. " counter lascia il campo. Danno: " .. damage_amount .. " al proprietario " .. p)
 		Duel.Damage(p,damage_amount,REASON_EFFECT)
 	end
 end
