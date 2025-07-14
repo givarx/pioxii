d48444d77033a8d5d02aeb6140ecd6ff19594bfc
__giva <157858211+givarx@users.noreply.github.com>
2025-07-14 12:31:08 +0200

    fix: remove unnecessary condition check for activation and simplify cost handling for special summoning "Colelli"

--- script/c600000031.lua
+++ script/c600000031.lua
@@ -12,7 +12,6 @@ function s.initial_effect(c)
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetCondition(s.condition)
     e1:SetCost(s.cost)
     e1:SetTarget(s.target)
     e1:SetOperation(s.activate)
@@ -20,38 +19,33 @@ function s.initial_effect(c)
 end
 s.listed_names={600000014} -- Colelli
 
--- Condizione: Controlla se c'è un "Davide" sul terreno
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.IsExistingMatchingCard(s.davidefilter,tp,LOCATION_MZONE,0,1,nil)
-end
 function s.davidefilter(c)
     return c:IsFaceup() and c:IsSetCard(0x4444) -- Archetipo "Davide"
 end
-
--- Costo: Scarta un mostro di tipo Macchina dalla mano
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.machinefilter,tp,LOCATION_HAND,0,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-    local g=Duel.SelectMatchingCard(tp,s.machinefilter,tp,LOCATION_HAND,0,1,1,nil)
-    Duel.SendtoGrave(g,REASON_COST)
-end
 function s.machinefilter(c)
-    return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_MACHINE) and c:IsAbleToGraveAsCost()
-end
-
--- Target: Controlla se puoi evocare un "Colelli"
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then 
-        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-            and Duel.IsExistingMatchingCard(s.colellifilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) 
-    end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+    return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_MACHINE) and c:IsDiscardable()
 end
 function s.colellifilter(c,e,tp)
     return c:IsCode(600000014,600000018) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) -- "Colelli"
 end
 
--- Operazione: Evoca specialmente un mostro con "Colelli" nel nome
+-- Costo: Scarta un mostro di tipo Macchina dalla mano
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.machinefilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+    Duel.DiscardHand(tp,s.machinefilter,1,1,REASON_COST|REASON_DISCARD)
+end
+
+-- Target: Controlla tutte le condizioni di attivazione
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then 
+        return Duel.IsExistingMatchingCard(s.davidefilter,tp,LOCATION_MZONE,0,1,nil)
+            and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+            and Duel.IsExistingMatchingCard(s.colellifilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) 
+    end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+end
+
+-- Operazione: Evoca specialmente un "Colelli"
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
     if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
