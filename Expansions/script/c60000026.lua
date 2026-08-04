f3c0ec6f478476d26d35136a6fa24331093abdd2
Giovanni De Cata s364170 <s312453@studenti.polito.it>
2026-08-04 19:46:47 +0200

    fix Piazza mercato

--- Expansions/script/c60000026.lua
+++ Expansions/script/c60000026.lua
@@ -1,5 +1,5 @@
 -- Script per "c600000026.lua"
-function c600000026.initial_effect(c)
+function c60000026.initial_effect(c)
     -- Attiva: se durante la Battle Phase subisci un attacco diretto e non controlli mostri, annulla l'attacco e 
     -- evoca 3 Segna•mostro "Grezzotto Di Candelaro" (TSO Obbligatorio/Down/Guerriero) in ATK, 
     -- il cui ATK è il danno che avresti ricevuto diviso 3.
@@ -7,13 +7,13 @@ function c600000026.initial_effect(c)
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e1:SetCondition(c600000026.condition)
-    e1:SetOperation(c600000026.operation)
+    e1:SetCondition(c60000026.condition)
+    e1:SetOperation(c60000026.operation)
     c:RegisterEffect(e1)
     
 end
 
-function c600000026.condition(e,tp,eg,ep,ev,re,r,rp)
+function c60000026.condition(e,tp,eg,ep,ev,re,r,rp)
     -- Verifica che l'attaccante appartenga all'avversario, che l'attacco sia diretto e che tu non controlli mostri.
     local a=Duel.GetAttacker()
     if a:IsControler(1-tp) then
@@ -22,7 +22,7 @@ function c600000026.condition(e,tp,eg,ep,ev,re,r,rp)
     return false
 end
 
-function c600000026.operation(e,tp,eg,ep,ev,re,r,rp)
+function c60000026.operation(e,tp,eg,ep,ev,re,r,rp)
     -- Annulla l'attacco
     local a=Duel.GetAttacker()
     local dam=a:GetAttack()
